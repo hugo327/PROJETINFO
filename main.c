e4c5a98e06d02e74a773c2b0c30995d3f8fe1956
hugo327 <74983394+hugo327@users.noreply.github.com>
2025-12-11 11:01:35 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "/home/blitz/Documents/C/PROJET/header.h"
+
+int main(){
+    char reponse[1];
+    printf("-----------------CANDY CRUSH----------------\n\n\n\n");
+    printf("utilisez les touches z q s d pour vous orientez, compris ?:(O/N) :");
+    fgets(reponse, 1, stdin);
+    if (reponse == 0)
+}
